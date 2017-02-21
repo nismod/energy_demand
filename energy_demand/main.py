956af2b88d700ddc29ef68c96afdbc42a86726d9
Sven Eggimann <cenv0553@OUCE.OX.AC.UK>
2017-02-21 16:22:16 +0000

    Updated old files with willl

--- energy_demand/main.py
+++ energy_demand/main.py
@@ -14,9 +14,10 @@
 #"""
 # pylint: disable=I0011,C0321,C0301,C0103, C0325
 
+#TODO Switch applian5ces and hours
 #TODO data Different appliances for cold/hot extremes
-#TODO data Heating fro min_max climate
-
+#TODO data Heating fro min_max climate 
+import sys
 import os
 import datetime
 from datetime import date
@@ -24,95 +25,6 @@ import numpy as np
 import main_functions as mf                         # Functions main module
 import residential_model                            # Residential sub-module
 import yaml
-import sys
-from datetime import date
-
-import main_functions as mf  # Functions main module
-import residential_model  # Residential sub-module
-
-print("Start Energy Demand Model")
-print("Python Version: " + str(sys.version))
-
-path_python = os.getcwd()                           # Get python path
-sys.path.append(path_python)                        # Append path
-sys.path.append(r'C:\Users\cenv0553\GIT\NISMODII')  # Append path to python files
-
-#import transition_module                           # Module containing different technology diffusion methods
-# ... more imports
-
-# Local paths #TODO: Reade from database? Read on server?
-path_main = r'C:\Users\cenv0553\GIT\NISMODII\data'
-path_pop_reg_lu = path_main + r'\scenario_and_base_data\lookup_nr_regions.csv'
-path_pop_reg_base = path_main + r'\scenario_and_base_data\population_regions.csv'
-path_dwelling_type_lu = path_main + r'\residential_model\lookup_dwelling_type.csv'
-path_lookup_appliances = path_main + r'\residential_model\lookup_appliances.csv'
-path_fuel_type_lu = path_main + r'\scenario_and_base_data\lookup_fuel_types.csv'
-path_day_type_lu = path_main + r'\residential_model\lookup_day_type.csv'
-path_seasons_lookup = path_main + r'\scenario_and_base_data\lookup_season.csv'
-
-path_base_elec_load_profiles = path_main + r'\residential_model\base_appliances_eletricity_load_profiles.csv'   # Path to base population
-path_base_data_fuel = path_main + r'\scenario_and_base_data\base_data_fuel.csv'                                 # Path to base fuel data
-path_temp_2015 = path_main + r'\residential_model\CSV_YEAR_2015.csv'                                            # Path to temperature data
-path_hourly_gas_shape = path_main +  r'\residential_model\residential_gas_hourly_shape.csv'                     # Path to hourly gas shape
-
-path_out_energy_supply_elec = r'C:\Users\cenv0553\GIT\NISMODII\model_output\to_energy_supply_elec.csv'      # Out path to energy supply model
-path_out_energy_supply_gas = r'C:\Users\cenv0553\GIT\NISMODII\model_output\to_energy_supply_gas.csv'        # Out path to energy supply model
-path_out_suppl_model = [path_out_energy_supply_elec, path_out_energy_supply_gas]
-
-# Global variables
-YEAR_SIMULATION = 2015                                                  # Provide year for which to run the simulation
-P1_YEAR_BASE = 2015                                                     # [int] First year of the simulation period
-P2_YEAR_END = 2050                                                      # [int] Last year of the simulation period
-P3_SIM_PERIOD = range(P2_YEAR_END - P1_YEAR_BASE)                       # List with simulation years
-P0_YEAR_CURR = YEAR_SIMULATION - P1_YEAR_BASE                           # [int] Current year in current simulation
-SIM_PARAM = [P0_YEAR_CURR, P1_YEAR_BASE, P2_YEAR_END, P3_SIM_PERIOD]    # Store all parameters in one list
-
-# Lookup tables
-reg_lu = mf.read_csv(path_pop_reg_lu)                      # Region lookup table
-dwelling_type_lu = mf.read_csv(path_dwelling_type_lu)      # Dwelling types lookup table
-app_type_lu = mf.read_csv(path_lookup_appliances)          # Appliances types lookup table
-fuel_type_lu = mf.read_csv(path_fuel_type_lu)              # Fuel type lookup
-day_type_lu = mf.read_csv(path_day_type_lu)                # Day type lookup
-season_lookup = mf.read_csv(path_seasons_lookup)           # Season lookup
-
-print(reg_lu)
-print(dwelling_type_lu)
-print(app_type_lu)
-print(fuel_type_lu)
-
-# Read in data
-# ------------------
-reg_pop = mf.read_csv(path_pop_reg_base, float)                  # Population data
-fuel_bd_data = mf.read_csv(path_base_data_fuel, float)          # All disaggregated fuels for different regions
-csv_temp_2015 = mf.read_csv(path_temp_2015)                     # csv_temp_2015
-hourly_gas_shape = mf.read_csv(path_hourly_gas_shape, float)   # Load hourly shape for gas from Robert Sansom
-# Read in more date such as floor area, nr of households etc. for base year # TODO
-
-#print("reg_pop:         " + str(reg_pop))
-#print("Fuel data:       " + str(fuel_bd_data))
-#print(hourly_gas_shape)  # Day, weekday, weekend
-
-print("-----------------------Start calculations----------------------------")
-
-# ---------------------------------------------------------------
-# Generate generic load profiles (shapes) [in %]
-# ---------------------------------------------------------------
-
-# Shape of base year for a full year for appliances (electricity) from HES data [%]
-shape_app_elec = mf.shape_bd_app(path_base_elec_load_profiles, day_type_lu, app_type_lu, SIM_PARAM[1])
-
-# Shape of base year for a full year for heating demand derived from XX [%]
-shape_hd_gas = mf.shape_bd_hd(csv_temp_2015, hourly_gas_shape)
-
-# Load more base demand
-# ...
-
-# ---------------------------------------------------------------
-# Base demand for the base year for all modelled elements
-# ---------------------------------------------------------------
-
-# Base demand of appliances over a full year (electricity)
-bd_app_elec = mf.bd_appliances(shape_app_elec, reg_lu, fuel_type_lu, fuel_bd_data)
 
 print("Start Energy Demand Model")
 print("Python Version: " + str(sys.version))
@@ -176,6 +88,7 @@ def load_data():
     #print("Fuel data:       " + str(fuel_bd_data))
     #print(hourly_gas_shape)  # Day, weekday, weekend
 
+    print("-----------------------Start calculations----------------------------")
     # ---------------------------------------------------------------
     # Generate generic load profiles (shapes) [in %]
     # ---------------------------------------------------------------
@@ -183,7 +96,6 @@ def load_data():
     # Shape of base year for a full year for appliances (electricity) from HES data [%]
     shape_app_elec = mf.shape_bd_app(path_base_elec_load_profiles, day_type_lu, app_type_lu, SIM_PARAM[1])
     print("Shape appliances")
-
     # Shape of base year for a full year for heating demand derived from XX [%]
     shape_hd_gas = mf.shape_bd_hd(csv_temp_2015, hourly_gas_shape)
 
@@ -232,7 +144,6 @@ def load_data():
     print("Sum Appliances simulation period:        " + str(timesteps_app_bd.sum()))
     print("Sum heating emand simulation period:     " + str(timesteps_hd_bd.sum()))
     print(" ")
-
     base_data = [SIM_PARAM, fuel_type_lu, dwelling_type_lu, reg_pop, fuel_bd_data, csv_temp_2015, hourly_gas_shape, shape_app_elec, shape_hd_gas, bd_app_elec, bd_hd_gas, timesteps_app_bd, timesteps_hd_bd, timesteps_own_selection]
 
     return base_data
@@ -279,7 +190,7 @@ def energy_demand_model(base_data, pop_data_external):
     '''
 
     # ---------------------------------------------------------------------------
-    # Generate the wrapper timesteps and instert data (disaggregate over full year)
+    # Generate the wrapper timesteps and add instert data
     # ---------------------------------------------------------------------------
 
     # Create timesteps for full year (wrapper-timesteps)
