Bug fixed in loading gas shapes. Improve peak calculation functions

energy_demand/data_loader_functions.py
@@ -118,56 +118,65 @@ def assign_carbon_trust_data_to_year(data, end_use, carbon_trust_data, base_year
 
     return shape_h_non_peak
 
-def get_hes_end_uses_shape(data, hes_data, year_raw_values, hes_y_peak, hes_y_warmest, end_use):
-    ''' read out end-use shape'''
-    # Calculate yearly total demand over all day years and all appliances
+def get_hes_end_uses_shape(data, year_raw_values, hes_y_peak, hes_y_warmest, end_use):
+    """Read in raw HES data and generate shapes
 
-    # -----------------------------------
-    # Peak calculation
-    # -----------------------------------
+    Calculate peak day demand, 
+
+    Parameters
+    ----------
+    data : data
+        Data container
+    year_raw_values : data
+        Unique dwellinge id
+    hes_y_peak : data
+        Unique dwellinge id
+    hes_y_warmest : data
+        Unique dwellinge id
+    end_use : data
+        Unique dwellinge id
+
+    Returns
+    -------
+    shape_d_peak : float
+        Peak day demand (Calculate factor which can be used to multiply yearly demand to generate peak demand)
+    shape_h_peak : float
+        Peak demand of each hours of peak day 
+
+    Notes
+    -----
     #Todo: Warmest load shape is not used
 
-    #-- daily peak
-    # Relationship of total yearly demand with averaged values and a peak day
+    shape_d_peak    To calculate actual energy demand, the yearly energy demand needs to be multiplied with this factor
+    shape_h_peak    Total sum is 1.0. To calculate actual energy demand, the peak day must be multiplied with this array
+
+    """
+    # Calculate yearly total demand over all day years and all appliances
+
+    # Peak calculation
+    # ----------------
+
+    #-- daily peak     # Relationship of total yearly demand with averaged values and a peak day
     appliances_HES = data['app_type_lu']
 
-
-    # If enduse is not in data
-    #print(data['lu_appliances_HES_matched'])
-    #if end_use not in data['lu_appliances_HES_matched'][:, 1]:
-    #    print("Enduse not HES data: " + str(end_use))
-    #    return data #, shape_h_peak, shape_h_non_peak, shape_d_peak, shape_d_non_peak
-
     # Get end use of HES data of current end_use of EUREC Data
-    for i in data['lu_appliances_HES_matched']: #TODO: Here the HES DATA ARE MACHTED
+    for i in data['lu_appliances_HES_matched']:
         if i[1] == end_use:
             hes_app_id = int(i[0])
             break
-            
-    # Select end use daily peak demand
+
+    # --Get peak daily load shape
+
+    # Calculate amount of energy demand for peak day of end_use
     peak_h_values = hes_y_peak[:, hes_app_id]
-    #print("peak_h_values: " + str(peak_h_values))
-
-    total_d_peak_demand = np.sum(peak_h_values)  # Peak is stored in JAN READ_IN HES DATA 2: PEak, 0: January, Select column
-
-    # Total yearly demand of end_use
-    total_y_end_use_demand = np.sum(year_raw_values[:, :, hes_app_id]) 
-
-    # Factor to calculate daily peak demand from total
-    shape_d_peak = (100/total_y_end_use_demand) * total_d_peak_demand
-
-    #print("total_d_peak_demand: " + str(total_d_peak_demand))
-    #print("total_y_end_use_demand: " + str(total_y_end_use_demand))
-    #print("yshape_d_peak: " + str(shape_d_peak))
-
-
-    # -----------------------------------
-    # Get peak daily load shape
-    # -----------------------------------
-
-    #-- hourly shape [% of total daily]
-    # 2: PEAK coldest daytype
-    shape_h_peak = hes_y_peak[:, hes_app_id] / total_d_peak_demand
+    total_d_peak_demand = np.sum(peak_h_values)
+    total_y_end_use_demand = np.sum(year_raw_values[:, :, hes_app_id]) # Calculate total yearly demand of end_use
+    
+    print("------------llllllllll")
+    print(total_y_end_use_demand)
+    print(total_d_peak_demand)
+    shape_d_peak = (1.0 / total_y_end_use_demand) * total_d_peak_demand # Factor to calculate daily peak demand from total
+    shape_h_peak = (1.0 / total_d_peak_demand) * peak_h_values # hourly values of peak day
 
     # -------------------------
     # Calculate non-peak shapes
@@ -176,13 +185,13 @@ def get_hes_end_uses_shape(data, hes_data, year_raw_values, hes_y_peak, hes_y_wa
     shape_h_non_peak = np.zeros((365, 24))
 
     for day in range(365):
-        d_sum = np.sum(year_raw_values[day, :, hes_app_id])
-        shape_d_non_peak[day] = (1 / total_y_end_use_demand) * d_sum
+        day_values = year_raw_values[day, :, hes_app_id]
+        d_sum = np.sum(day_values)
+        
+        shape_d_non_peak[day] = (1.0 / total_y_end_use_demand) * d_sum
+        shape_h_non_peak[day] = (1.0 / d_sum) * day_values # daily shape
 
-        # daily shape
-        shape_h_non_peak[day] = (1 / d_sum) * year_raw_values[day, :, hes_app_id]
-
-    return data, shape_h_peak, shape_h_non_peak, shape_d_peak, shape_d_non_peak
+    return shape_h_peak, shape_h_non_peak, shape_d_peak, shape_d_non_peak
 
 # CWV WEATER GAS SAMSON-----------------------------------
 def read_shp_heating_gas(data, model_type, wheater_scenario):
@@ -195,8 +204,9 @@ def read_shp_heating_gas(data, model_type, wheater_scenario):
 
     #TODO: THIS CAN BE USED TO DERIVED temp_2015_non_residential_gas data
     """
+    all_demand_values = [] # Store all calculated data to select maximum energy use for peak shape
     # Initilaise array to store all values for a year
-    hourly_hd = np.zeros((1, 24), dtype=float)
+    #hourly_hd = np.zeros((1, 24), dtype=float)
     hd_data = np.zeros((365, 24), dtype=float)
 
     hourly_gas_shape = mf.read_csv_float(data['path_dict']['path_hourly_gas_shape_resid']) / 100 # Because given in percentages (division no inlfuence on results as relative anyway)
@@ -242,9 +252,10 @@ def read_shp_heating_gas(data, model_type, wheater_scenario):
         # Distribute daily deamd into hourly demand
         if weekday == 5 or weekday == 6:
             hd_data[yearday_python] = hourly_gas_shape_wkend * heating_demand_correlation
+            all_demand_values.append(hourly_gas_shape_wkend * heating_demand_correlation)
         else:
             hd_data[yearday_python] = hourly_gas_shape_wkday * heating_demand_correlation
-
+            all_demand_values.append(hourly_gas_shape_wkday * heating_demand_correlation)
     # NON-PEAK------------------------------------------------------------------------------
 
     # --hourly
@@ -270,14 +281,17 @@ def read_shp_heating_gas(data, model_type, wheater_scenario):
 
     # PEAK-----------------------------------------------------------------------------
 
-    # Get maximum daily demand
-    _list = []
-    for demand in data['path_temp_2015']:
-        _list.append(float(demand[1]))
-    max_day_demand = max(_list) #maximum demand
+    # ITerate all days and select day with most fuels (sum across all fueltypes) as peak day
+    max_day_demand = 0
+    for day_fuels in all_demand_values:
+        if np.sum(day_fuels)> np.sum(max_day_demand):
+            max_day_demand = np.sum(day_fuels) #maximum demand
 
     # Factor to calc maximum daily peak #TODO: INclude uncertainty?
     shape_d_peak = max_day_demand / total_y_hd
+
+    print("Daily load shape gas loading----" + str(wheater_scenario))
+    print(shape_d_peak)
 
     return shape_h_peak, shape_h_non_peak, shape_d_peak, shape_d_non_peak
 
@@ -371,8 +385,7 @@ def read_raw_carbon_trust_data(data, folder_path):
                     if is_leap_year(int(year)) == True:
                         year = year + 1 # Shift whole dataset to another year
                         if month == 2 and day == 29:
-                            #print("Skip loeap day")
-                            continue
+                            continue #skip leap day
 
                     date_row = date(year, month, day) #Date
                     daytype = mf.get_weekday_type(date_row) # Daytype
@@ -560,11 +573,14 @@ def create_txt_shapes(end_use, path_txt, shape_h_peak, shape_h_non_peak, shape_d
     #print(shape_h_non_peak.shape)   # 365, 24
     #print(shape_d_peak.shape)       # ()
     #print(shape_d_non_peak.shape)   # 365, 1
-    jason_to_txt_shape_h_peak(shape_h_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_h_peak') + str(other_string_info) + str('.txt')))
-    jason_to_txt_shape_h_non_peak(shape_h_non_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_h_non_peak') + str(other_string_info) + str('.txt')))
-    jason_to_txt_shape_d_peak(shape_d_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_d_peak') + str(other_string_info) + str('.txt')))
+    jason_to_txt_shape_h_peak(shape_h_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_h_peak') + str('.txt')))
+    jason_to_txt_shape_h_non_peak(shape_h_non_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_h_non_peak') + str('.txt')))
+    jason_to_txt_shape_d_peak(shape_d_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_d_peak') + str('.txt')))
     jason_to_txt_shape_d_non_peak(shape_d_non_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_d_non_peak') + str('.txt')))
-
+    #jason_to_txt_shape_h_peak(shape_h_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_h_peak') + str(other_string_info) + str('.txt')))
+    ##jason_to_txt_shape_h_non_peak(shape_h_non_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_h_non_peak') + str(other_string_info) + str('.txt')))
+    #jason_to_txt_shape_d_peak(shape_d_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_d_peak') + str(other_string_info) + str('.txt')))
+    #jason_to_txt_shape_d_non_peak(shape_d_non_peak, os.path.join(path_txt, str(end_use) + str("__") + str('shape_d_non_peak') + str('.txt')))
 def jason_to_txt_shape_h_peak(input_array, outfile_path):
     """Wrte to txt. Array with shape: (24,)"""
     np_dict = dict(enumerate(input_array))

energy_demand/residential_model.py
@@ -112,7 +112,7 @@ class Region(object):
         self.data = data                                            # data
         self.data_ext = data_ext                                    # external data
         self.assumptions = data['assumptions']
-        self.current_yr = data_ext['glob_var']['current_yr']    # Current year
+        self.current_yr = data_ext['glob_var']['current_yr']        # Current year
         self.reg_fuel = data['fueldata_disagg'][reg_id]             # Fuel array of region (used to extract all end_uses)
 
         # Set attributs of all enduses
@@ -120,12 +120,15 @@ class Region(object):
 
         # Sum final 'yearly' fuels (summarised over all enduses)
         self.fuels_new = self.tot_all_enduses_y()
+        print("----------dddddd")
+        for fueltype in self.fuels_new:
+            print(np.sum(fueltype)/365)
 
         # Get 'peak demand day' (summarised over all enduses)
-        self.enduse_fuel_peak_d = self.get_enduse_peak_d()
+        self.fuels_peak_d = self.get_enduse_peak_d()
 
         # Get 'peak demand h' (summarised over all enduses)
-        self.enduse_fuel_peak_h = self.get_enduse_peak_h()
+        self.fuels_peak_h = self.get_enduse_peak_h()
 
         # Sum 'daily' demand in region (summarised over all enduses)
         self.fuels_tot_enduses_d = self.tot_all_enduses_d()
@@ -137,9 +140,13 @@ class Region(object):
         np.testing.assert_almost_equal(np.sum(self.fuels_tot_enduses_d), np.sum(self.fuels_tot_enduses_h), err_msg='The Regional disaggregation from d to h is false')
         # add some more
 
-        # Calculate load factors
+        # Calculate load factors from peak values
         self.reg_load_factor_d = self.load_factor_d()
+        print("ZEB:" + str(self.reg_load_factor_d))
+
         self.reg_load_factor_h = self.load_factor_h()
+        print("ZEB3:" + str(self.reg_load_factor_h))
+        #prnt("-.")
 
         # Plot stacked end_uses
         #start_plot = mf.convert_date_to_yearday(2015, 1, 1) #regular day
@@ -177,7 +184,7 @@ class Region(object):
         return sum_fuels_d
 
     def get_enduse_peak_d(self):
-        """Summarise peak value of all end_uses"""
+        """Summarise absolute fuel of peak days over all end_uses"""
         sum_enduse_peak_d = np.zeros((len(self.data['fuel_type_lu']), 1))  # Initialise
 
         for enduse in self.reg_fuel:
@@ -206,51 +213,78 @@ class Region(object):
         return sum_fuels_h
 
     def load_factor_d(self):
-        """Calculate load factor of a day in a year"""
-        lf_y = np.zeros((len(self.data['fuel_type_lu']), 1)) # Initialise
+        """Calculate load factor of a day in a year from peak values
 
-        maximum_d = self.enduse_fuel_peak_d
+        self.fuels_peak_d     :   Fuels for peak day (fueltype, data)
+        self.fuels_tot_enduses_d    :   Hourly fuel for different fueltypes (fueltype, 24 hours data)
 
+        Return
+        ------
+        lf_d : array
+            Array with load factor for every fuel type in %
+
+        Info
+        -----
+        Load factor = average load / maximum load in given time period
+
+        Info: https://en.wikipedia.org/wiki/Load_factor_(electrical)
+
+        #TODO: calculate also not from peak values
+        """
+        lf_d = np.zeros((len(self.data['fuel_type_lu']), 1))
+
+        # Get day with maximum demand (in percentage of year)
+        peak_d_demand = self.fuels_peak_d
+
+
+        #prnt("oko")
         # Iterate fueltypes to calculate load factors for each fueltype
         for k, data_fueltype in enumerate(self.fuels_tot_enduses_d):
+            average_demand = np.sum(data_fueltype) / 365 # Averae_demand = yearly demand / nr of days
+            print("average_demand: " + str(average_demand))
+            print("peak_d_demand: " + str(peak_d_demand[k]))
+            print("---")
 
-            # Averae load = yearly demand / nr of days
-            average_demand = np.sum(data_fueltype) / 365
+            lf_d[k] = average_demand / peak_d_demand[k] # Calculate load factor
+        
+        # Convert load factor to %
+        lf_d = lf_d * 100
 
-            # Calculate load factor
-            lf_y[k] = average_demand / maximum_d[k]
-
-        return lf_y
+        return lf_d
 
     def load_factor_h(self):
         """Calculate load factor of a h in a year
-        # TODO: PEAK CAlculations are still wrong
 
-        # Note retirmd as [%]...
+        self.fuels_peak_h     :   Fuels for peak day (fueltype, data)
+        self.fuels_tot_enduses_d    :   Hourly fuel for different fueltypes (fueltype, 24 hours data)
+
+        Return
+        ------
+        lf_h : array
+            Array with load factor for every fuel type [in %]
+
+        Info
+        -----
+        Load factor = average load / maximum load in given time period
+
+        Info: https://en.wikipedia.org/wiki/Load_factor_(electrical)
+
+        #TODO: calculate also not from peak values
         """
-        # Initialise array to store fuel
-        lf_y = np.zeros((len(self.data['fuel_type_lu']), 1))
+        lf_h = np.zeros((len(self.data['fuel_type_lu']), 1)) # Initialise array to store fuel
 
         # Iterate fueltypes to calculate load factors for each fueltype
         for fueltype, data_fueltype in enumerate(self.fuels_tot_enduses_h):
-            #print("FUELTYPE: " + str(fueltype))
-            maximum_h_of_day = np.amax(self.enduse_fuel_peak_h[fueltype])
-
-            #print("maximum_h_of_day: " + str(maximum_h_of_day))
+            maximum_h_of_day = np.amax(self.fuels_peak_h[fueltype])
 
             # If there is a maximum day hour
             if maximum_h_of_day != 0:
-
-                # Averae load = yearly demand / nr of days
-                average_demand_h = np.sum(data_fueltype) / (365 * 24)
-                #print("data_fueltype:    " + str(np.sum(data_fueltype)))
-                #print("average_demand_h: " + str(average_demand_h))
-
-                # Calculate load factor
-                lf_y[fueltype] = average_demand_h / maximum_h_of_day
-
-        #print("lf_y: " + str(lf_y))
-        return lf_y
+                average_demand_h = np.sum(data_fueltype) / (365 * 24) # Averae load = yearly demand / nr of days
+                lf_h[fueltype] = average_demand_h / maximum_h_of_day # Calculate load factor
+        
+        # Convert load factor to %
+        lf_h = lf_h * 100
+        return lf_h
 
     def __getattr__(self, attr):
         """Get method of own object"""
@@ -381,6 +415,7 @@ class EndUseClassResid(object): #OBJECT OR REGION? --> MAKE REGION IS e.g. data 
 
         # --Yearly fuel data (Check if always function below takes result from function above)
         print("ENDUSE: " + str(self.enduse))
+        print(data['dict_shp_enduse_d_resid']['heating']['shape_d_peak'])
 
         self.reg_fuel_eff_gains = self.enduse_eff_gains()                # General efficiency gains of technology over time
         print("AAA: " + str(np.sum(self.reg_fuel_eff_gains)))
@@ -390,17 +425,23 @@ class EndUseClassResid(object): #OBJECT OR REGION? --> MAKE REGION IS e.g. data 
 
         self.reg_fuel_after_elasticity = self.enduse_elasticity()        # Calculate demand with changing elasticity (elasticity maybe on household level with floor area)
         print("ccc: " + str(np.sum(self.reg_fuel_after_elasticity)))
-
+        
         self.reg_fuelscen_driver = self.enduse_scenario_driver()         # Calculate new fuel demands after scenario drivers TODO: THIS IS LAST MUTATION IN PROCESS... (all disaggreagtion function refer to this)
         print("DDD: " + str(np.sum(self.reg_fuelscen_driver)))
-
+        print(self.enduse_shape_peak_d)
 
         # --Daily fuel data
         self.reg_fuel_d = self.enduse_y_to_d()                           # Disaggregate yearly demand for every day
 
         # --Hourly fuel data
         self.enduse_fuel_h = self.enduse_d_to_h()                        # Disaggregate daily demand to hourly demand
+        
+        # --Peak data
         self.enduse_fuel_peak_d = self.enduse_peak_d()                   # Calculate peak day
+        print("SS")
+        print(data['dict_shp_enduse_d_resid']['heating']['shape_d_peak'])
+        print(self.enduse_fuel_peak_d)
+        #prnt("..")
         self.enduse_fuel_peak_h = self.enduse_peak_h()                   # Calculate peak hour
 
         # Testing
@@ -652,7 +693,7 @@ class EndUseClassResid(object): #OBJECT OR REGION? --> MAKE REGION IS e.g. data 
         return fuels_h
 
     def enduse_peak_d(self):
-        """Disaggregate yearly fuel data to the peak day.
+        """Disaggregate yearly absolute fuel data to the peak day.
 
         Parameters
         ----------
@@ -662,16 +703,26 @@ class EndUseClassResid(object): #OBJECT OR REGION? --> MAKE REGION IS e.g. data 
         Returns
         -------
         fuels_d_peak : array
-            Hourly fuel data [fueltypes, peak_day, hours]
+            Hourly absolute fuel data
 
-        Notes
+        Example
         -----
+
+        Input: 20 FUEL * 0.004 [0.4%] --> new fuel 
+
         """
         fuels_d_peak = np.zeros((len(self.reg_fuel), 1))
+        print("...ddd")
+        print(self.enduse)
+        print(self.reg_fuelscen_driver)
+        print("---")
+        print(self.enduse_shape_peak_d)
 
         # Iterate yearday and
         for k, fueltype_year_data in enumerate(self.reg_fuelscen_driver):
-            fuels_d_peak[k] = self.enduse_shape_peak_d * fueltype_year_data[0] # enduse_shape_d is  a two dim array with load shapes in first row
+            fuels_d_peak[k] = self.enduse_shape_peak_d * fueltype_year_data[0]
+            #print("elf.enduse_shape_peak_d * fueltype_year_data[0]")
+            #print(self.enduse_shape_peak_d * fueltype_year_data[0])
 
         return fuels_d_peak
 