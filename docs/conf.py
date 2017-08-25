48077488a9eafefe7242f48e2a8eb8d414730cf3
Sven Eggimann <sven.eggimann@ouce.ox.ac.uk>
2017-08-25 19:21:15 +0100

    test "../"

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ if on_rtd:
 
     output_dir = os.path.join(__location__, "../docs/api")
     #module_dir = os.path.join(__location__, "../energy_demand")
-    module_dir = os.path.join(__location__, "../") 
+    module_dir = os.path.join(__location__, "../") #"../""
     cmd_line_template = "sphinx-apidoc -f -o {outputdir} {moduledir}"
     cmd_line = cmd_line_template.format(outputdir=output_dir, moduledir=module_dir)
     apidoc.main(cmd_line.split(" "))
