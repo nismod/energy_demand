0328cd9589f8d5ea7ef207b48a4afec79e4c039d
Will Usher <william.usher@ouce.ox.ac.uk>
2017-08-31 12:46:00 +0100

    Added cli to energy demand model and console command ed

--- energy_demand/__init__.py
+++ energy_demand/__init__.py
@@ -1,1 +1,12 @@
-__all__ = []
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Energy Demand
+"""
+from __future__ import division, print_function, absolute_import
+
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except:
+    __version__ = 'unknown'
