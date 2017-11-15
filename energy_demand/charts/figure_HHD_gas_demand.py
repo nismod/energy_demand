- correct gwh and gw units in plots - change that all missing geocodes in joining are better shown

energy_demand/charts/figure_HHD_gas_demand.py
@@ -3,18 +3,18 @@
 Calculate HDD with weather data from a asingle weather station for the whole of the UK.abs
 Correlate HDD with national gas data.
 
-National gas data source: National Grid (2015) Seasonal Normal Demand Forecasts
+National gas data source: National Grid (2015) Seasonal Normal Demand Forecasts¨
 
-Note: This could be improved by calculating the HDD for every region and multiplying
-by the population. This would take into consideration the different population
+http://www2.nationalgrid.com/UK/Industry-information/Gas-transmission-operational-data/Supplementary-Reports/
+
 """
 import logging
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
-from energy_demand.scripts import s_raw_weather_data
 from energy_demand.plotting import plotting_program
 from energy_demand.geography.weather_station_location import get_closest_station
+from energy_demand.basic import conversions
 
 def main(region_names, weather_regions, data):
     """Plot weighted HDD (HDD per Region & region pop)
@@ -43,10 +43,9 @@ def main(region_names, weather_regions, data):
             # HDD
             reg_hdd_day = closest_weather_region.rs_hdd_by[day]
             reg_pop = data['scenario_data']['population'][base_yr][region_name] 
-            #logging.warning("POP  {}   {}  {}".format(reg_pop, reg_hdd_day, closest_weather_region))
+
             #Weighted HDD
             weighted_hdd = reg_hdd_day * reg_pop
-
             weighted_daily_HDD[reg_array][day] += weighted_hdd
 
     # Sum across regions
@@ -55,46 +54,12 @@ def main(region_names, weather_regions, data):
     # Convert to list
     weighted_daily_HDD = list(weighted_daily_HDD)
 
-    '''path_data_temp = os.path.abspath(
-        "C:\DATA_NISMODII\data_energy_demand\_raw_data\H-Met_office_weather_data\midas_wxhrly_201501-201512.csv")
-
-    path_data_stations = os.path.abspath(
-        "C:\DATA_NISMODII\data_energy_demand\_raw_data\H-Met_office_weather_data\excel_list_station_details.csv")
-
-    # Read temp data
-    temp_data_raw = s_raw_weather_data.read_weather_data_raw(path_data_temp, 9999)
-
-    # Clean raw temperature data
-    temp_data = s_raw_weather_data.clean_weather_data_raw(temp_data_raw, 9999)
-
-    # Weather stations
-    weather_stations = s_raw_weather_data.read_weather_stations_raw(path_data_stations, temp_data.keys())
-
-    # Temperature weather data weater station 
-    #593	ELMDON	WEST	MIDLANDS	COUNTY	01/01/1949	Current	52.4524	-1.74099	B26	3				--> slightly better correlation
-    station_ID_ELMDON = 593 #593
-    temperatures = temp_data[station_ID_ELMDON]
-
-    # Calculate average day temp
-    averag_day_temp = []
-    for day in temperatures:
-        averag_day_temp.append(np.mean(day))
-
-    # ----------------------------------
-    # Calculate HDD
-    # ----------------------------------
-    t_base_heating = 15.5 # Heating t_base temp
-
-    # HDD
-    hdd_reg = hdd_cdd.calc_hdd(t_base_heating, temperatures)
-
-    # Test if correlation with mean temp is better than with HDd
-    #hdd_reg = averag_day_temp
-    '''
     # -- Non daily metered gas demand in mcm == Residential heating gas 
     # demand for year 2015 (Jan - Dez --> Across two excel in orig file)
     # gas demand for 365 days
-    gas_demand_NDM_2015_2016 = [
+
+    # Unit: GWh per day
+    gas_demand_NDM_2015_2016_gwh = [
         2059.3346672,
         2170.0185108,
         2098.5700609,
@@ -462,6 +427,382 @@ def main(region_names, weather_regions, data):
         1997.4328038
         ]
 
+    # Total SND (includes IUK exports and storage injection)
+    gas_demand_TOTALSND_2015_2016 = [
+        3017,
+        3218,
+        3093,
+        3105,
+        3281,
+        3281,
+        3280,
+        3279,
+        3246,
+        3089,
+        3101,
+        3277,
+        3278,
+        3278,
+        3276,
+        3242,
+        3085,
+        3095,
+        3270,
+        3269,
+        3267,
+        3267,
+        3235,
+        3081,
+        3093,
+        3270,
+        3267,
+        3264,
+        3261,
+        3226,
+        3063,
+        3066,
+        3234,
+        3226,
+        3221,
+        3216,
+        3179,
+        3020,
+        3035,
+        3217,
+        3222,
+        3227,
+        3233,
+        3207,
+        3056,
+        3073,
+        3246,
+        3241,
+        3237,
+        3233,
+        3188,
+        3023,
+        3022,
+        3185,
+        3170,
+        3155,
+        3139,
+        3088,
+        2919,
+        2904,
+        3058,
+        3037,
+        3016,
+        2994,
+        2941,
+        2773,
+        2764,
+        2915,
+        2897,
+        2885,
+        2872,
+        2828,
+        2673,
+        2669,
+        2826,
+        2816,
+        2805,
+        2792,
+        2746,
+        2594,
+        2586,
+        2736,
+        2720,
+        2705,
+        2690,
+        2645,
+        2496,
+        2486,
+        2635,
+        2626,
+        2597,
+        2588,
+        2537,
+        2311,
+        2300,
+        2527,
+        2541,
+        2526,
+        2510,
+        2476,
+        2340,
+        2321,
+        2458,
+        2436,
+        2413,
+        2391,
+        2339,
+        2193,
+        2175,
+        2315,
+        2302,
+        2287,
+        2274,
+        2230,
+        2098,
+        2084,
+        2223,
+        2211,
+        2200,
+        2190,
+        2142,
+        1975,
+        1961,
+        2009,
+        2064,
+        2050,
+        2036,
+        2006,
+        1885,
+        1871,
+        2035,
+        2022,
+        2012,
+        2002,
+        1967,
+        1848,
+        1834,
+        1969,
+        1961,
+        1952,
+        1945,
+        1908,
+        1795,
+        1778,
+        1848,
+        1888,
+        1881,
+        1874,
+        1850,
+        1746,
+        1738,
+        1872,
+        1867,
+        1862,
+        1857,
+        1825,
+        1721,
+        1711,
+        1845,
+        1842,
+        1839,
+        1836,
+        1803,
+        1700,
+        1689,
+        1821,
+        1817,
+        1814,
+        1811,
+        1778,
+        1677,
+        1667,
+        1801,
+        1799,
+        1797,
+        1796,
+        1766,
+        1667,
+        1657,
+        1788,
+        1786,
+        1708,
+        1705,
+        1674,
+        1584,
+        1573,
+        1693,
+        1691,
+        1690,
+        1688,
+        1659,
+        1571,
+        1562,
+        1682,
+        1681,
+        1679,
+        1678,
+        1650,
+        1563,
+        1553,
+        1673,
+        1672,
+        1670,
+        1669,
+        1637,
+        1550,
+        1540,
+        1660,
+        1659,
+        1657,
+        1656,
+        1628,
+        1542,
+        1533,
+        1653,
+        1653,
+        1652,
+        1654,
+        1626,
+        1541,
+        1531,
+        1649,
+        1648,
+        1647,
+        1646,
+        1619,
+        1534,
+        1526,
+        1646,
+        1646,
+        1648,
+        1650,
+        1625,
+        1540,
+        1534,
+        1657,
+        1659,
+        1661,
+        1663,
+        1638,
+        1551,
+        1545,
+        1669,
+        1670,
+        1672,
+        1675,
+        1651,
+        1564,
+        1560,
+        1691,
+        1697,
+        1703,
+        1709,
+        1688,
+        1602,
+        1601,
+        1735,
+        1742,
+        1748,
+        1754,
+        1733,
+        1643,
+        1643,
+        1779,
+        1784,
+        1792,
+        1803,
+        1783,
+        1692,
+        1698,
+        1843,
+        1855,
+        1867,
+        2041,
+        2009,
+        1868,
+        1873,
+        2089,
+        2103,
+        2119,
+        2132,
+        2102,
+        1958,
+        1968,
+        2188,
+        2206,
+        2224,
+        2242,
+        2212,
+        2063,
+        2074,
+        2296,
+        2303,
+        2312,
+        2324,
+        2288,
+        2130,
+        2143,
+        2369,
+        2385,
+        2404,
+        2422,
+        2395,
+        2243,
+        2261,
+        2491,
+        2508,
+        2524,
+        2541,
+        2514,
+        2357,
+        2376,
+        2612,
+        2623,
+        2633,
+        2649,
+        2619,
+        2457,
+        2481,
+        2725,
+        2743,
+        2758,
+        2771,
+        2731,
+        2552,
+        2563,
+        2796,
+        2801,
+        2812,
+        2824,
+        2791,
+        2618,
+        2642,
+        2893,
+        2911,
+        2923,
+        2935,
+        2899,
+        2716,
+        2730,
+        2979,
+        2993,
+        3002,
+        3008,
+        2969,
+        2777,
+        2788,
+        3035,
+        3042,
+        3047,
+        3059,
+        3024,
+        2835,
+        2847,
+        3033,
+        3041,
+        3050,
+        2907,
+        2758,
+        2710,
+        2715,
+        2816,
+        2929,
+        2930,
+        2932,
+    ]
+    #gas_demand_NDM_2015_2016_gwh = gas_demand_TOTALSND_2015_2016
+
+    # -------------------------
+    # Convert GWh per day to GW
+    # -------------------------
+    # Conversions
+    gas_demand_NDM_2015_2016_gw = list(conversions.gewhperday_to_gw(np.array(gas_demand_NDM_2015_2016_gwh)))
+
     # ----------------
     # Linear regression
     # ----------------
@@ -469,9 +810,8 @@ def main(region_names, weather_regions, data):
         y = slope * x + intercept
         return y
 
-    #slope, intercept, r_value, p_value, std_err = stats.linregress(gas_demand_NDM_2015_2016, hdd_reg)
     slope, intercept, r_value, p_value, std_err = stats.linregress(
-        gas_demand_NDM_2015_2016,
+        gas_demand_NDM_2015_2016_gw,
         weighted_daily_HDD)
 
     logging.warning("Slope:         %s", str(slope))
@@ -486,40 +826,37 @@ def main(region_names, weather_regions, data):
     # Set figure size in cm
     plt.figure(figsize=plotting_program.cm2inch(8, 8))
 
-    # plot points
-    #plt.plot(gas_demand_NDM_2015_2016, hdd_reg, 'ro', markersize=5, color='gray')
-
     plt.plot(
-        gas_demand_NDM_2015_2016,
+        gas_demand_NDM_2015_2016_gw,
         weighted_daily_HDD,
         'ro',
         markersize=3.5,
         color='gray')
 
-    # plot line
-    #plt.plot(gas_demand_NDM_2015_2016, hdd_reg, 'ro')
-
     # ---------------------
     # Plot regression line
     # ---------------------
-    x_plot = np.linspace(300, 2250, 500)
+    x_plot = np.linspace(10, 90, 500)
     y_plot = []
     for x in x_plot:
         y_plot.append(lin_func(x, slope, intercept))
 
-    plt.plot(x_plot, y_plot, color='k')
+    plt.xlim(0, 100)
+    plt.plot(
+        x_plot,
+        y_plot,
+        color='k')
 
     # ---------------------
     # Labelling
     # ---------------------
     font_additional_info = {'family': 'arial', 'color': 'black', 'weight': 'normal', 'size': 8}
-    plt.xlabel("National daily gas demand [GWh per day]")
-    plt.ylabel("Heating degree days [mio]")
-    plt.title("LAD HDD * LADPOP vs Gas_y_uk (r_value: {}".format(round(r_value, 3)), fontdict=font_additional_info)
+    plt.xlabel("UK non daily metered gas demand [GW]")
+    plt.ylabel("HDD * POP [mio]")
+    plt.title("LAD_HDD * LAD_POP vs Gas_y_uk (r_value: {})".format(round(r_value, 3)), fontdict=font_additional_info)
     #plt.text(10, 10, "y = {} x + {}".format(round(slope, 2), round(intercept, 2)), fontdict=font_additional_info)
 
     # Tight layout
     #plt.tight_layout()
     plt.margins(x=0)
-
     plt.show()

energy_demand/plotting/plotting_results.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.patches as mpatches
 from energy_demand.plotting import plotting_program
-from energy_demand.basic import basic_functions
+from energy_demand.basic import basic_functions, conversions
 from matplotlib.patches import Rectangle
 
 # INFO
@@ -26,11 +26,12 @@ def run_all_plot_functions(
     ):
     """Summary function to plot all results
 
-
     """
     logging.info("... plotting results")
 
-    # All enduses
+    # ------------------------------
+    # Plot annual demand for enduses
+    # ------------------------------
     plt_stacked_enduse_sectors(
         lookups,
         sim_param['simulated_yrs'],
@@ -83,7 +84,7 @@ def run_all_plot_functions(
         lookups,
         os.path.join(
             local_paths['data_results_PDF'],
-            'fig_tot_all_enduse01.pdf'))
+            'y_fueltypes_all_enduses.pdf'))
 
     # --------------
     # Fuel week of base year
@@ -183,8 +184,8 @@ def plot_seasonal_lf(fueltype_int, fueltype_str, load_factors_seasonal, reg_nrs,
     # Settings
     color_list = {
         'winter': 'midnightblue',
-        'summer': 'darkgreen',
-        'spring': 'springgreen',
+        'summer': 'olive',
+        'spring': 'darkgreen',
         'autumn': 'gold'}
 
     classes = list(color_list.keys())
@@ -235,6 +236,14 @@ def plot_seasonal_lf(fueltype_int, fueltype_str, load_factors_seasonal, reg_nrs,
             linewidth=1,
             linestyle='--',
             alpha=1.0)
+        
+        # Plot markers for average line
+        plt.plot(
+            years,
+            average_season_year_years,
+            color=color_list[season],
+            markersize=0.5,
+            marker='o')
 
     # -----------------
     # Axis
@@ -272,6 +281,7 @@ def plot_seasonal_lf(fueltype_int, fueltype_str, load_factors_seasonal, reg_nrs,
         recs,
         classes,
         ncol=2,
+        prop={'family': 'arial','size': 8},
         loc='best',
         frameon=False)
 
@@ -306,7 +316,11 @@ def plot_lf_y(fueltype_int, fueltype_str, load_factors_y, reg_nrs, path_plot_fig
             x_values_year.append(year)
             y_values_year.append(lf_fueltype_reg[fueltype_int][reg_nr])
 
-        plt.plot(x_values_year, y_values_year, color='grey')
+        plt.plot(
+            x_values_year,
+            y_values_year,
+            linewidth=0.2,
+            color='grey')
 
     # -----------------
     # Axis
@@ -339,7 +353,7 @@ def plot_lf_y(fueltype_int, fueltype_str, load_factors_y, reg_nrs, path_plot_fig
     plt.savefig(path_plot_fig)
     plt.close()
 
-def plt_stacked_enduse(sim_period, results_enduse_every_year, enduses_data, fig_name):
+def plt_stacked_enduse(years_simulated, results_enduse_every_year, enduses_data, fig_name):
     """Plots stacked energy demand
 
     Arguments
@@ -356,8 +370,6 @@ def plt_stacked_enduse(sim_period, results_enduse_every_year, enduses_data, fig_
 
     # INFO Cannot plot a single year?
     """
-    years_simulated = sim_period
-
     x_data = years_simulated
     y_data = np.zeros((len(enduses_data), len(years_simulated)))
 
@@ -365,7 +377,11 @@ def plt_stacked_enduse(sim_period, results_enduse_every_year, enduses_data, fig_
     for fueltype_int, enduse in enumerate(enduses_data):
         legend_entries.append(enduse)
         for model_year, data_model_run in enumerate(results_enduse_every_year.values()):
-            y_data[fueltype_int][model_year] = np.sum(data_model_run[enduse])
+
+            # Conversion: Convert GWh per years to GW
+            yearly_sum_gw = conversions.gwhperyear_to_gw(np.sum(data_model_run[enduse]))
+
+            y_data[fueltype_int][model_year] = yearly_sum_gw
 
     # Set figure size
     fig = plt.figure(figsize=plotting_program.cm2inch(8, 8))
@@ -407,7 +423,6 @@ def plt_stacked_enduse(sim_period, results_enduse_every_year, enduses_data, fig_
     # Get color of stacks in stackplot
     color_stackplots = [mpl.patches.Rectangle((0, 0), 0, 0, facecolor=pol.get_facecolor()[0]) for pol in stack_plot]
 
-    #BELUGA
     '''plt.legend(
         color_stackplots,
         legend_entries,
@@ -421,6 +436,7 @@ def plt_stacked_enduse(sim_period, results_enduse_every_year, enduses_data, fig_
         legend_entries,
         loc='upper center',
         bbox_to_anchor=(0.5, -0.05),
+        prop={'family': 'arial','size': 8},
         frameon=False,
         shadow=True,
         ncol=4)
@@ -433,9 +449,9 @@ def plt_stacked_enduse(sim_period, results_enduse_every_year, enduses_data, fig_
     # -------
     # Labels
     # -------
-    plt.ylabel("energy demand [GWh per year]")
-    plt.xlabel("years")
-    plt.title("energy demand for years for whole UK")
+    plt.ylabel("GW")
+    plt.xlabel("year")
+    plt.title("ED whole UK")
 
     # Tight layout
     plt.tight_layout()
@@ -445,8 +461,17 @@ def plt_stacked_enduse(sim_period, results_enduse_every_year, enduses_data, fig_
     plt.savefig(fig_name)
     plt.close()
 
-def plt_stacked_enduse_sectors(lookups, sim_period, results_enduse_every_year, rs_enduses, ss_enduses, is_enduses, fig_name):
-    """Plots summarised endues for the three sectors
+def plt_stacked_enduse_sectors(
+        lookups,
+        years_simulated,
+        results_enduse_every_year,
+        rs_enduses,
+        ss_enduses,
+        is_enduses,
+        fig_name
+    ):
+    """Plots summarised endues for the three sectors. Annual 
+    GWh are converted into GW.
 
     Arguments
     ----------
@@ -462,7 +487,6 @@ def plt_stacked_enduse_sectors(lookups, sim_period, results_enduse_every_year, r
 
     # INFO Cannot plot a single year?
     """
-    years_simulated = sim_period
     x_data = years_simulated
     nr_submodels = 3
     y_data = np.zeros((nr_submodels, len(years_simulated)))
@@ -477,18 +501,26 @@ def plt_stacked_enduse_sectors(lookups, sim_period, results_enduse_every_year, r
         submodel = 0
         for fueltype_int in range(lookups['fueltypes_nr']):
             for enduse in rs_enduses:
-                y_data[submodel][model_year] += np.sum(data_model_run[enduse][fueltype_int])
+
+                # Conversion: Convert gwh per years to gw
+                yearly_sum_gw = conversions.gwhperyear_to_gw(np.sum(data_model_run[enduse][fueltype_int]))
+                y_data[submodel][model_year] += yearly_sum_gw
 
         submodel = 1
         for fueltype_int in range(lookups['fueltypes_nr']):
             for enduse in ss_enduses:
-                y_data[submodel][model_year] += np.sum(data_model_run[enduse][fueltype_int])
+
+                # Conversion: Convert gwh per years to gw
+                yearly_sum_gw = conversions.gwhperyear_to_gw(np.sum(data_model_run[enduse][fueltype_int]))
+                y_data[submodel][model_year] += yearly_sum_gw
 
         submodel = 2
         for fueltype_int in range(lookups['fueltypes_nr']):
             for enduse in is_enduses:
-                y_data[submodel][model_year] += np.sum(data_model_run[enduse][fueltype_int])
 
+                # Conversion: Convert gwh per years to gw
+                yearly_sum_gw = conversions.gwhperyear_to_gw(np.sum(data_model_run[enduse][fueltype_int]))
+                y_data[submodel][model_year] += yearly_sum_gw
 
 
     ##import matplotlib.colors as colors #for color_name in colors.cnmaes:
@@ -534,6 +566,7 @@ def plt_stacked_enduse_sectors(lookups, sim_period, results_enduse_every_year, r
         recs,
         leg_labels,
         ncol=1,
+        prop={'family': 'arial','size': 8},
         loc='best',
         frameon=False)
 
@@ -545,8 +578,8 @@ def plt_stacked_enduse_sectors(lookups, sim_period, results_enduse_every_year, r
     # -------
     # Labels
     # -------
-    plt.ylabel("energy demand [GWh per year]")
-    plt.xlabel("years")
+    plt.ylabel("GW")
+    plt.xlabel("year")
     plt.title("UK ED per sector")
 
     # Tight layout
@@ -601,8 +634,17 @@ def plot_load_curves_fueltype(results_objects, data, fig_name):
     plt.savefig(fig_name)
     plt.close()
 
-def plt_fuels_enduses_week(results_resid, lookups, nr_of_h_to_plot, model_yeardays_nrs, year_to_plot, fig_name):
-    """Plots stacked end_use for all regions
+def plt_fuels_enduses_week(
+        results_resid,
+        lookups,
+        nr_of_h_to_plot,
+        model_yeardays_nrs,
+        year_to_plot,
+        fig_name
+    ):
+    """Plots stacked end_use for all regions. As
+    input GWh per h are provided, which cancels out to
+    GW.
 
     Input
     -----
@@ -642,13 +684,14 @@ def plt_fuels_enduses_week(results_resid, lookups, nr_of_h_to_plot, model_yearda
     for day in range(model_yeardays_nrs):
         x_tick_pos.append(day * 24)
     plt.xticks(x_tick_pos, days_to_plot, color='black')
+    
     plt.axis('tight')
 
     plt.legend(ncol=2, frameon=False)
 
-    plt.ylabel("energy demand [GWh per hour]")
-    plt.xlabel("days")
-    plt.title("Total yearly fuels of all enduses per fueltype for simulation year {}".format(year_to_plot + 2050))
+    plt.ylabel("GW")
+    plt.xlabel("day")
+    plt.title("tot annual ED, all enduses, fueltype {}".format(year_to_plot + 2050))
 
     # Saving figure
     plt.savefig(fig_name)
@@ -656,7 +699,8 @@ def plt_fuels_enduses_week(results_resid, lookups, nr_of_h_to_plot, model_yearda
 
 def plt_fuels_enduses_y(results_resid, lookups, fig_name):
     """Plot lines with total energy demand for all enduses
-    per fueltype over the simluation period
+    per fueltype over the simluation period. Annual GWh
+    are converted into GW.
 
     Arguments
     ---------
@@ -682,12 +726,12 @@ def plt_fuels_enduses_y(results_resid, lookups, fig_name):
         # Read out fueltype specific max h load
         data_years = {}
         for year, data_year in results_resid.items():
-            tot_fuel_fueltype_y = np.sum(data_year[fueltype_int])
+            tot_gwh_fueltype_y = np.sum(data_year[fueltype_int])
 
-            # -------
-            # Conversion i necessary?? TODO
-            # -------
-            data_years[year] = tot_fuel_fueltype_y
+            #Conversion: Convert gwh per years to gw
+            yearly_sum_gw = conversions.gwhperyear_to_gw(tot_gwh_fueltype_y)
+
+            data_years[year] = yearly_sum_gw
 
         y_values_fueltype[fueltype_str] = data_years
 
@@ -720,21 +764,30 @@ def plt_fuels_enduses_y(results_resid, lookups, fig_name):
             color=color_line,
             label=fueltype_str)
 
+    # ----
+    # Axis
+    # ----
+    plt.xlim(xmin=0) #no upper limit to xmax
+
     # ------------
     # Plot legend
     # ------------
-    plt.legend(ncol=2, loc=2, frameon=False)
+    plt.legend(
+        ncol=2,
+        loc=2,
+        prop={'family': 'arial','size': 8},
+        frameon=False)
 
     # ---------
     # Labels
     # ---------
-    plt.ylabel("energy demand [GWh per year]")
-    plt.xlabel("years")
-    plt.title("total yearly fuels of all enduses per fueltype")
+    plt.ylabel("GW")
+    plt.xlabel("year")
+    plt.title("tot annual ED per fueltype")
 
     # Tight layout
     plt.tight_layout()
-    plt.margins(x=0)#ax.margins(x=0)
+    plt.margins(x=0)
 
     # Save fig
     plt.savefig(fig_name)
@@ -785,7 +838,10 @@ def plt_fuels_peak_h(tot_fuel_dh_peak, lookups, path_plot_fig):
         plt.plot(years, y_init[fueltype])
 
     # Legend
-    ax.legend(legend_entries)
+    ax.legend(
+        legend_entries,
+        prop={'family': 'arial','size': 8},
+        frameon=False)
 
     # -
     # Axis
@@ -800,15 +856,15 @@ def plt_fuels_peak_h(tot_fuel_dh_peak, lookups, path_plot_fig):
 
     # Minor ticks
     minor_ticks = np.arange(base_yr, years[-1] + minor_interval, minor_interval)
-    ax.set_xticks(minor_ticks, minor = True)
+    ax.set_xticks(minor_ticks, minor=True)
 
     plt.xlim(2015, years[-1])
 
     # --------
     # Labeling
     # --------
-    plt.ylabel("energy demand [GWh per hour]")
-    plt.xlabel("years")
+    plt.ylabel("GW")
+    plt.xlabel("year")
     plt.title("ED peak hour, y, all enduses and regions")
 
     # Tight layout
@@ -828,7 +884,8 @@ def plot_load_profile_dh_multiple(
         plot_peak=False,
         plot_all_entries=False
     ):
-    """Plotting average saisonal loads for each daytype
+    """Plotting average saisonal loads for each daytype. As an input
+    GWh is provided, which for each h is cancelled out to GW.
 
     https://stackoverflow.com/questions/4325733/save-a-subplot-in-matplotlib
     """
@@ -847,8 +904,15 @@ def plot_load_profile_dh_multiple(
             # Plot average
             # ------------------
             x_values = range(24)
-            plt.plot(x_values, list(calc_av_lp_real[season][daytype]), color='green', label='real_by')
-            plt.plot(x_values, list(calc_av_lp_modelled[season][daytype]), color='red', label='modelled_future')
+            plt.plot(
+                x_values,
+                list(calc_av_lp_real[season][daytype]),
+                color='green', label='real_by')
+
+            plt.plot(
+                x_values,
+                list(calc_av_lp_modelled[season][daytype]),
+                color='red', label='modelled_future')
 
             # ------------------
             # Plot every single line
@@ -866,7 +930,12 @@ def plot_load_profile_dh_multiple(
                 # Get row with maximum hourly value
                 day_with_max_h = np.argmax(np.max(calc_lp_real[season][daytype], axis=1))
 
-                plt.plot(x_values, list(calc_lp_real[season][daytype][day_with_max_h]), color='orange', markersize=1.0, label='peak')
+                plt.plot(
+                    x_values,
+                    list(calc_lp_real[season][daytype][day_with_max_h]),
+                    color='orange',
+                    markersize=1.0,
+                    label='peak')
 
             # -----------------
             # Axis
@@ -891,12 +960,16 @@ def plot_load_profile_dh_multiple(
             plt.text(1, 0.55, "RMSE: {}".format(round(rmse, 2)), fontdict=font_additional_info)
             plt.title(title_info, loc='left', fontdict=font_additional_info)
             #plt.ylabel("hours")
-            #plt.ylabel("average electricity [GW] ")
+            #plt.ylabel("average electricity [GW]")
 
     # ------------
     # Plot legend
     # ------------
-    plt.legend(ncol=1, loc=2, fontsize=8, frameon=False)
+    plt.legend(
+        ncol=1,
+        loc=2,
+        prop={'family': 'arial','size': 8}, #fontsize=8,
+        frameon=False)
 
     # Tight layout
     plt.tight_layout()

energy_demand/profiles/load_profile.py
@@ -448,16 +448,24 @@ def calc_av_lp(demand_yh, seasons, model_yeardays_daytype):
 
     """
     season_daytypes = {
-        'spring': {'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
-        'summer': {'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
-        'autumn': {'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
-        'winter': {'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)}}
+        'spring': {
+            'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
+        'summer': {
+            'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
+        'autumn': {
+            'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
+        'winter': {
+            'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)}}
 
     av_season_daytypes = {
-        'spring': {'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
-        'summer': {'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
-        'autumn': {'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
-        'winter': {'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)}}
+        'spring': {
+            'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
+        'summer': {
+            'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
+        'autumn': {
+            'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)},
+        'winter': {
+            'workday': np.zeros((0, 24), dtype=float), 'holiday': np.zeros((0, 24), dtype=float)}}
 
     for yearday, daytype_yearday in enumerate(model_yeardays_daytype):
 

energy_demand/read_write/write_data.py
@@ -4,7 +4,7 @@ import os
 import logging
 import numpy as np
 import configparser
-from energy_demand.basic import basic_functions
+from energy_demand.basic import basic_functions, conversions
 from energy_demand.geography import write_shp
 import yaml
 from yaml import Loader, Dumper
@@ -13,8 +13,9 @@ import collections
 def write_pop(sim_yr, path_result, pop_y):
     """Write scenario population for a year
     """
-    # Write to txt
-    path_file = os.path.join(path_result, "model_run_pop", "pop__{}__{}".format(sim_yr, ".txt"))
+    path_file = os.path.join(
+        path_result,
+        "model_run_pop", "pop__{}__{}".format(sim_yr, ".txt"))
 
     np.savetxt(path_file, pop_y, delimiter=',')
 
@@ -70,9 +71,12 @@ def create_shp_results(data, results_container, paths, lookups, lu_reg):
             # Calculate yearly sum
             yearly_sum = np.sum(results_container['results_every_year'][year][fueltype], axis=1)
 
+            # Conversion: Convert gwh per years to gw
+            yearly_sum_gw = conversions.gwhperyear_to_gw(yearly_sum)
+
             field_names.append('y_{}_{}'.format(year, fueltype))
             csv_results.append(
-                basic_functions.array_to_dict(yearly_sum, lu_reg))
+                basic_functions.array_to_dict(yearly_sum_gw, lu_reg))
 
         # Add population
         field_names.append('pop_{}'.format(year))