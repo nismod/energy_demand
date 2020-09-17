50a33d1a961e8257ddc4d3d83ec89d9b19709639
Tom Russell <tomalrussell@gmail.com>
2020-09-17 16:40:42 +0100

    Always use conda in CI

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -4,55 +4,51 @@
 # The behavior of the script is controlled by environment variabled defined
 # in the .travis.yml in the top level folder of the project.
 #
-# This script is taken from Scikit-Learn (http://scikit-learn.org/)
-#
-# THIS SCRIPT IS SUPPOSED TO BE AN EXAMPLE. MODIFY IT ACCORDING TO YOUR NEEDS!
+# This script is adapted from Scikit-Learn (http://scikit-learn.org/)
 
 set -e
 
-if [[ "$DISTRIB" == "conda" ]]; then
-    # Deactivate the travis-provided virtual environment and setup a
-    # conda-based environment instead
-    deactivate
+# Deactivate the travis-provided virtual environment and setup a
+# conda-based environment instead
+deactivate
 
-    # Use the miniconda installer for faster download / install of conda
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-        -O miniconda.sh
-    chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
-    export PATH=$HOME/miniconda/bin:$PATH
+# Use the miniconda installer for faster download / install of conda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+    -O miniconda.sh
+chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
+export PATH=$HOME/miniconda/bin:$PATH
 
-    # Don't ask for confirmation
-    conda config --set always_yes true
-    # Update conda
-    conda update conda
+# Don't ask for confirmation
+conda config --set always_yes true
+# Update conda
+conda update conda
 
-    # Follow channel priority strictly
-    conda config --set channel_priority strict
-    # Don't change prompt
-    conda config --set changeps1 false
+# Follow channel priority strictly
+conda config --set channel_priority strict
+# Don't change prompt
+conda config --set changeps1 false
 
-    # Add conda-forge as priority channel
-    conda config --add channels conda-forge
+# Add conda-forge as priority channel
+conda config --add channels conda-forge
 
-    # Configure the conda environment using package names (using .environment.yml would force
-    # the python version)
-    conda create -n testenv \
-        python=$PYTHON_VERSION \
-        fiona \
-        geopandas \
-        haversine \
-        imageio \
-        matplotlib \
-        numpy \
-        palettable \
-        pandas \
-        pyproj \
-        pytest \
-        pytest-cov \
-        scipy
+# Configure the conda environment using package names (using .environment.yml would force
+# the python version)
+conda create -n testenv \
+    python=$PYTHON_VERSION \
+    fiona \
+    geopandas \
+    haversine \
+    imageio \
+    matplotlib \
+    numpy \
+    palettable \
+    pandas \
+    pyproj \
+    pytest \
+    pytest-cov \
+    scipy
 
-    source activate testenv
-fi
+conda activate testenv
 
 pip install coverage coveralls
 
