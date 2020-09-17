4e7bf869e9108b2e8ad596817f4cd50ccc9e04da
Tom Russell <tomalrussell@gmail.com>
2020-09-17 16:44:56 +0100

    Set conda command

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -17,7 +17,8 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
     -O miniconda.sh
 chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
 export PATH=$HOME/miniconda/bin:$PATH
-
+# Setup conda command
+conda init bash
 # Don't ask for confirmation
 conda config --set always_yes true
 # Update conda
